2911380dba0bd49614727dca164d3a6db8f19235
Cem GEÇGEL <gecgelcem@outlook.com>
2026-05-10 21:27:42 +0300

    Add `cmake/external/Yyjson.cmake`

---
+++ cmake/external/Yyjson.cmake
@@ -0,0 +1,5 @@
+add_subdirectory(
+    "${CMAKE_SOURCE_DIR}/downloads/yyjson"
+    "${CMAKE_BINARY_DIR}/downloads/yyjson"
+    EXCLUDE_FROM_ALL
+)
--- sources/main.c
+++ sources/main.c
@@ -1,3 +1,29 @@
+#include <assert.h>
 #include <stdio.h>
+#include <yyjson.h>
 
-int main() { puts("Hello Jello!"); }
+int main() {
+  yyjson_read_err err;
+  yyjson_doc *doc =
+      yyjson_read_file("examples/entrypoint/jello.json", 0, NULL, &err);
+  assert(doc);
+
+  yyjson_val *root = yyjson_doc_get_root(doc);
+  yyjson_val *compilerTests = yyjson_obj_get(root, "compilerTests");
+  size_t idx, max;
+  yyjson_val *compilerTest;
+  yyjson_arr_foreach(compilerTests, idx, max, compilerTest) {
+    printf("compiler test #%zu: args: [", idx);
+
+    yyjson_val *args = yyjson_obj_get(compilerTest, "args");
+    size_t idxArgs, maxArgs;
+    yyjson_val *arg;
+    yyjson_arr_foreach(args, idxArgs, maxArgs, arg) {
+      printf("%s%s", idxArgs ? ", " : "", yyjson_get_str(arg));
+    }
+
+    printf("]\n");
+  }
+
+  yyjson_doc_free(doc);
+}
